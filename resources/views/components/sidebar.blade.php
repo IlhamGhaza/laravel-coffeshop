567badffa5c75ba989b0e6649c4e72533952f6ba
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-02-13 19:29:57 +0700

    product

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">RESTO Ilham</a>
+            <a href="index.html">Coffe Ilham</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">RG</a>
+            <a href="index.html">RB</a>
         </div>
         <ul class="sidebar-menu">
 
@@ -19,6 +19,20 @@
                 <ul class="dropdown-menu">
                     <li class=''>
                         <a class="nav-link" href="{{ route('users.index') }}">Users</a>
+                    </li>
+
+                </ul>
+
+                <ul class="dropdown-menu">
+                    <li class=''>
+                        <a class="nav-link" href="{{ route('products.index') }}">Products</a>
+                    </li>
+
+                </ul>
+
+                <ul class="dropdown-menu">
+                    <li class=''>
+                        <a class="nav-link" href="{{ route('categories.index') }}">Categories</a>
                     </li>
 
                 </ul>
