348e795094290e5a85050b34a99557df65032067
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-07-04 23:54:14 +0700

    update resources/views/pages/inventory/index.blade.php

--- resources/views/pages/inventory/index.blade.php
+++ resources/views/pages/inventory/index.blade.php
@@ -15,7 +15,7 @@
                 <div class="section-header-button">
                     <a href="{{ route('inventory.create') }}" class="btn btn-primary">Add New</a>
                 </div>
-                
+
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                     <div class="breadcrumb-item"><a href="#">Inventory</a></div>
@@ -80,7 +80,7 @@
                                                 </td>
                                                 <td>
                                                     {{ $inventory->unit }}
-
+                                                </td>
                                                     <div class="d-flex justify-content-center">
                                                         <a href='{{ route('inventory.edit', $inventory->id) }}'
                                                             class="btn btn-sm btn-info btn-icon">
